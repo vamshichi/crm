4934d0024c030e24baffacf4397b2fa83d00185e
unknown <chvamshi03@gmail.com>
2025-02-20 19:22:33 +0530

    second commit

--- app/api/employee/[id]/route.ts
+++ app/api/employee/[id]/route.ts
@@ -1,18 +1,28 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import prisma from "@/app/lib/prisma";
 
-export async function GET() {
+export async function GET(req: NextRequest, { params }: any) {
   try {
-    const employees = await prisma.employee.findMany({
+    const { employeeId } = params;
+    if (!employeeId) {
+      return NextResponse.json({ error: "Missing employee ID" }, { status: 400 });
+    }
+
+    const employee = await prisma.employee.findUnique({
+      where: { id: employeeId },
       include: {
         department: true,
         leads: true,
       },
     });
 
-    return NextResponse.json(employees, { status: 200 });
+    if (!employee) {
+      return NextResponse.json({ error: "Employee not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(employee, { status: 200 });
   } catch (error) {
-    console.error("Error fetching employees:", error);
-    return NextResponse.json({ message: "Internal server error" }, { status: 500 });
+    console.error("Error fetching employee:", error);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   }
 }
