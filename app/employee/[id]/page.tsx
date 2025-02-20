019bac8c45c5e39c43d8c1a8d64e8fac634f01c0
unknown <chvamshi03@gmail.com>
2025-02-20 19:40:55 +0530

    second commit

--- app/employee/[id]/page.tsx
+++ app/employee/[id]/page.tsx
@@ -1,38 +1,39 @@
-import prisma from '@/app/lib/prisma';
-import { notFound } from 'next/navigation';
-import EmployeeLeads from '@/app/components/leads/getleads';
-import LeadForm from '@/app/components/employee/LeadForm';
+// import prisma from '@/app/lib/prisma';
+// import { notFound } from 'next/navigation';
+// import EmployeeLeads from '@/app/components/leads/getleads';
+// import LeadForm from '@/app/components/employee/LeadForm';
 
-export default async function EmployeePage(params : { id: string }) {
-  if (!params.id) {
-    return notFound();
-  }
+export default async function EmployeePage (){
+//   if (!params.id) {
+//     return notFound();
+//   }
 
-  const employee = await prisma.employee.findUnique({
-    where: { id: params.id },
-    include: { department: true },
-  });
+  // const employee = await prisma.employee.findUnique({
+  //   where: { id: params.id },
+  //   include: { department: true },
+  // });
 
-  if (!employee) {
-    return notFound();
-  }
+  // if (!employee) {
+  //   return notFound();
+  // }
 
   return (
     <div className="container mx-auto p-4">
       <h2 className="text-2xl font-semibold mb-4">
-        Welcome to Your Dashboard, {employee.name}
+        {/* Welcome to Your Dashboard, {employee.name} */}
       </h2>
       <p>
-        <strong>Email:</strong> {employee.email}
+        {/* <strong>Email:</strong> {employee.email} */}
       </p>
       <p>
-        <strong>Role:</strong> {employee.role}
+        {/* <strong>Role:</strong> {employee.role} */}
       </p>
       <p>
-        <strong>Project:</strong> {employee.department?.name || 'No department'}
+        {/* <strong>Project:</strong> {employee.department?.name || 'No department'} */}
       </p>
-      <LeadForm />
-      <EmployeeLeads employeeId={employee.id} />
+      
+      {/* <LeadForm />
+      <EmployeeLeads employeeId={employee.id} /> */}
     </div>
   );
 }
